916dc64f51aa42084db3949b4b0a410a2b6a07ac
Chanoon Na <chanoonna@gmail.com>
2023-04-07 18:21:24 -0700

    Restructure Introduction.tsx

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -3,9 +3,9 @@ import './Introduction.scss';
 export const Introduction = () => {
   return (
     <section className="introduction-container">
-      <h1>Hi, my name is Chanoon.</h1>
-      <h2 className="role">I am a Frontend developer,</h2>
-      <h2 className="welcome">Welcome to my portfolio!</h2>
+      <h1>Chanoon Na,</h1>
+      <h2 className="role">Front-end developer</h2>
+      <p className="welcome">Hi, I write code and find coding to be fun.</p>
     </section>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,6 @@ import './NavBar.scss';
 export const NavBar = () => {
   return (
     <nav className="navbar-container">
-      <p className="nav-name">Chanoon Na</p>
       <ul className="nav-item-container">
         <li className="nav-item">WORK</li>
         <li className="nav-item">PROJECTS</li>
