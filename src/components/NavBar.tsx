1d21e56ed687b6bd620c4d7028bd2ee6275d67cc
Chanoon Na <chanoonna@gmail.com>
2023-04-10 15:28:57 -0700

    Link github and linkedin

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,6 @@
 import './NavBar.scss';
+import { LinkedInIcon } from './shared/LinkedInIcon';
+import { GithubIcon } from './shared/GithubIcon';
 
 export const NavBar = () => {
   return (
@@ -7,8 +9,25 @@ export const NavBar = () => {
         <li className="nav-item">WORK</li>
         <li className="nav-item">PROJECTS</li>
         <li className="nav-item">ABOUT ME</li>
-        <li className="nav-item">RESUME</li>
-        <li className="nav-item">CONTACT</li>
+        <li
+          className="nav-item"
+          onClick={() => {
+            window.open('https://github.com/chanoonna', '_blank');
+          }}
+        >
+          <GithubIcon width="25" height="25" />
+        </li>
+        <li
+          className="nav-item"
+          onClick={() => {
+            window.open(
+              'https://www.linkedin.com/in/chanoon-na-2482a6208',
+              '_blank'
+            );
+          }}
+        >
+          <LinkedInIcon />
+        </li>
       </ul>
     </nav>
   );
---
+++ src/components/shared/LinkedInIcon.tsx
@@ -0,0 +1,8 @@
+import './shared.scss';
+import LinkedInImage from '../../assets/images/LI-In-Bug.png';
+
+export const LinkedInIcon = () => (
+  <div className="linked-in-icon-container">
+    <img src={LinkedInImage} alt="LinkedIn" />
+  </div>
+);
