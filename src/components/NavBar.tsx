176a8987612e3b17b07b3088661728c7efc9322b
Chanoon Na <chanoonna@gmail.com>
2023-04-10 16:11:32 -0700

    Update NavBar items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,14 +8,17 @@ export const NavBar = () => {
       <ul className="nav-item-container">
         <li className="nav-item">WORK</li>
         <li className="nav-item">PROJECTS</li>
-        <li className="nav-item">ABOUT ME</li>
+        <li className="nav-item">CV</li>
+        <li className="nav-item">
+          <a href="mailto:chanoonna@gmail.com">EMAIL</a>
+        </li>
         <li
           className="nav-item"
           onClick={() => {
             window.open('https://github.com/chanoonna', '_blank');
           }}
         >
-          <GithubIcon width="25" height="25" />
+          <GithubIcon width="30" height="30" />
         </li>
         <li
           className="nav-item"
