cb71f56bf73f01714dd97c3c01eb4bbf1c128f82
Chanoon Na <chanoonna@gmail.com>
2023-04-10 19:58:17 -0700

    Add sticky effect when scrolled for navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,29 @@
 import './NavBar.scss';
+import { useState, useEffect } from 'react';
 import { LinkedInIcon } from './shared/LinkedInIcon';
 import { GithubIcon } from './shared/GithubIcon';
 
 export const NavBar = () => {
+  const [isOnTop, setIsOnTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const innerHeight = window.innerHeight;
+      const yMargin = innerHeight * 0.05;
+      if (window.scrollY <= yMargin) {
+        setIsOnTop(true);
+      } else {
+        setIsOnTop(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
-    <nav className="navbar-container">
-      <ul className="nav-item-container">
-        <li className="nav-item">WORK</li>
-        <li className="nav-item">PROJECTS</li>
-        <li className="nav-item">CV</li>
-        <li className="nav-item">
-          <a href="mailto:chanoonna@gmail.com">EMAIL</a>
-        </li>
+    <nav className={'navbar-container' + (isOnTop ? ' on-top' : '')}>
+      <ul className="nav-item-container left">
         <li
           className="nav-item"
           onClick={() => {
@@ -30,6 +42,15 @@ export const NavBar = () => {
           }}
         >
           <LinkedInIcon />
+        </li>
+      </ul>
+      <ul className="nav-item-container right">
+        <li className="nav-item">WORK</li>
+        <li className="nav-item">PROJECTS</li>
+        <li className="nav-item">ABOUT ME</li>
+        <li className="nav-item">CV</li>
+        <li className="nav-item">
+          <a href="mailto:chanoonna@gmail.com">EMAIL</a>
         </li>
       </ul>
     </nav>
