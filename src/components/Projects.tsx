ec2982135cb2a5383639e2da420f099384eba0b2
Chanoon Na <chanoonna@gmail.com>
2023-04-13 10:47:33 -0700

    Update Projects description and add more images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -100,7 +100,7 @@ const PROJECTS: IProject[] = [
     id: 3,
     name: 'Point of sale (Ongoing)',
     description:
-      'An electron desktop application that allows users to manage their sales, inventory. Users can create, edit, and delete sales and their products. It also visualizes user data with multiple types of charts.',
+      'Electron desktop application for general Point of Sale application. The goal of the project is to make sales simple and easy and visualizing the sales data so that users can see how their business is doing.',
     imageLinks: [
       'https://github.com/chanoonna/pos-app/blob/main/docs/Screenshot%202023-04-04%20at%204.25.53%20PM.png?raw=true',
       'https://github.com/chanoonna/pos-app/blob/main/docs/Screenshot%202023-04-04%20at%204.26.47%20PM.png?raw=true',
@@ -120,9 +120,10 @@ const PROJECTS: IProject[] = [
     id: 2,
     name: 'Open house of commons',
     description:
-      'A project with both frontend and backend. The frontend is a multi-page react application that allows users to search for their MPs and see their voting records by visualizing voting data. The backend is a node.js API server that downloads data from the official parliament website, parses the data, and stores it to postgres database. The frontend and backend are connected through REST API.',
+      'Project with multi-page react app for the front-end and back-end with express. The app visualizes how MPs of Canadian parliament voted on different bills. Back-end data is from the official data from the parliament website provided in xml format. The app downloads all the files, parce them, and store them in the database.',
     imageLinks: [
-      'https://github.com/csb3/open-house-of-commons/raw/543ae6128638889587b7fca3c8ce877aaabfa6fa/docs/vote-view.png?raw=true'
+      'https://raw.githubusercontent.com/csb3/open-house-of-commons/543ae6128638889587b7fca3c8ce877aaabfa6fa/docs/homepage.png',
+      'https://raw.githubusercontent.com/csb3/open-house-of-commons/543ae6128638889587b7fca3c8ce877aaabfa6fa/docs/vote-view.png'
     ],
     github: 'https://github.com/chanoonna/open-house-of-commons',
     stacks: ['React', 'chart.js', 'sass', 'node.js', 'express', 'postgresql']
@@ -131,9 +132,11 @@ const PROJECTS: IProject[] = [
     id: 1,
     name: 'Interview schedulear',
     description:
-      'A single page react application that allows users to schedule interviews with real-time updates by using websocket. Users can create, edit, and delete interviews. Users can also switch between days and see the schedule for each day.',
+      'A single page react application that allows users to schedule interviews. The app uses websocket for real-time updates. Users can create, edit, delete interviews.',
     imageLinks: [
-      'https://raw.githubusercontent.com/chanoonna/scheduler/master/docs/bookinterview.png'
+      'https://raw.githubusercontent.com/chanoonna/scheduler/master/docs/displayinterview.png',
+      'https://raw.githubusercontent.com/chanoonna/scheduler/master/docs/bookinterview.png',
+      'https://raw.githubusercontent.com/chanoonna/scheduler/master/docs/cancelinterview.png'
     ],
     github: 'https://github.com/chanoonna/scheduler',
     stacks: ['React', 'sass', 'jest', 'cypress', 'circleci', 'storybook']
--- src/components/constants.ts
+++
@@ -1,3 +0,0 @@
-export const ID_CAREER = 'career-section';
-export const ID_PROJECTS = 'projects-section';
-export const ID_ABOUT_ME = 'about_me-section';
