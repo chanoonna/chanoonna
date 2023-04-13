8846581bd90ec0777c469eb59541ddfb0dd3f9db
Chanoon Na <chanoonna@gmail.com>
2023-04-12 17:27:37 -0700

    Adjust height of Introduction

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -3,11 +3,13 @@ import './Introduction.scss';
 export const Introduction = () => {
   return (
     <section className="introduction-container">
-      <h1>Chanoon Na,</h1>
-      <h2 className="role">Front-end developer</h2>
-      <div className="comment-container">
-        <p>Hi, I write code and I love doing it.</p>
-        <span className="prompt">|</span>
+      <div className="introduction-main">
+        <h1>Chanoon Na,</h1>
+        <h2 className="role">Front-end developer</h2>
+        <div className="comment-container">
+          <p>Hi, I write code and I love doing it.</p>
+          <span className="prompt">|</span>
+        </div>
       </div>
     </section>
   );
