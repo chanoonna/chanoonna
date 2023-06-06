8a9c43dadffeeef6703d6dbe92bfb27cc998ff01
Chanoon Na <chanoonna@gmail.com>
2023-06-05 22:56:26 -0700

    Update introduction page

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,10 +5,14 @@ export const Introduction = () => {
     <section className="introduction-container">
       <div className="introduction-main">
         <h1>Chanoon Na,</h1>
-        <h2 className="role">Front-end developer</h2>
+        <h2 className="role">Software developer</h2>
+        <br />
         <div className="comment-container">
-          <p>Hi, I write code and I love doing it.</p>
-          <span className="prompt">|</span>
+          <p>
+            Transforming Digital Dreams into Visual Reality: I enjoy creating
+            empowering user experiences through web development.
+            <span className="prompt">|</span>
+          </p>
         </div>
       </div>
     </section>
