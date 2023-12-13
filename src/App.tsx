6f2f5d522002b088b936dfe8e2cb5d28f58f9546
Chanoon Na <chanoonna@gmail.com>
2023-12-12 17:02:09 -0800

    Remove

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { NavBar, Introduction, Career, Projects, AboutMe } from 'components';
 export const App = () => {
   return (
     <div className="app-container">
-      <NavBar />
       <Introduction />
-      <Career />
-      <Projects />
-      <AboutMe />
     </div>
   );
 };
--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,16 +4,9 @@ export const Introduction = () => {
   return (
     <section className="introduction-container">
       <div className="introduction-main">
-        <h1>Chanoon Na,</h1>
-        <h2 className="role">Software developer</h2>
-        <br />
-        <div className="comment-container">
-          <p>
-            Transforming Digital Dreams into Visual Reality: I enjoy creating
-            empowering user experiences through web development.
-            <span className="prompt">|</span>
-          </p>
-        </div>
+        <p>
+          <span className="prompt">|</span>
+        </p>
       </div>
     </section>
   );
